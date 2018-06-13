3c8d3b6b89448581859752e481b1854ceaa4cb38
l0o0 <linxzh1989@gmail.com>
2018-06-13 18:15:49 +0800

    Change version to 0.0.2

--- rosaceae/__init__.py
+++ rosaceae/__init__.py
@@ -2,3 +2,5 @@
 from . import bins
 from . import scorecard
 from . import utils
+
+__version__ = '0.0.2'
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 setup(
     name='rosaceae',
-    version='0.0.1',
+    version='0.0.2',
     description='Python pacakge for credit risk scorecards',
     long_description=readme(),
     author='l0o0',
