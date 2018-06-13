25c948b668ccfe81f0745df1b9a87cedc8e4da73
l0o0 <linxzh1989@gmail.com>
2018-06-13 15:05:18 +0800

    Add woe_iv function, delete functions about iv and woe wroten before.

--- rosaceae/scorecard.py
+++ rosaceae/scorecard.py
@@ -10,65 +10,32 @@ import pandas as pd
 
 from math import log, e
 
-from .bins import bin_tree
-
-
-# 计算 woe 值
-def getWOE(x, y, bins_out=None, good_label=0, verbose=False):
-    '''Calculate WOE value.
-    WOE(weight of evidence)
-    1 indicates good case, 0 indicates bad case.
-
-    Args:
-        -x : array like.
-        -y : array like, target value.
-
-    Returns:
-    '''
-    total_good = float(sum(y==good_label))
-    total_bad = y.shape[0] - total_good
-    out = {}
-
-    if bins_out == None:
-        if verbose:
-            print("Binning data by rosaceae.bins.bin_tree")
-        bins_out = bin_tree(x, y)
-
-    for border in bins_out:
-        border_good = float(sum(y[bins_out[border]]==good_label))
-        border_bad = len(bins_out[border]) - border_good
-        border_woe = log((border_bad/total_bad)/(border_good/total_good))
-        out[border] = border_woe        
-    return out
-
-
-def getWOE_a(vars, y, data, good_label=0, verbose=False, table=False):
-    '''A wrapper of getWOE
-    Calculate woe for var in vars. Vars should be in data's columns.
-    A dict with var and its output from getWOE is returned.
-    '''
-    out = {}
-    for v in vars:
-        v_out = getWOE(data[v], data[y], good_label=good_label, verbose=verbose)
-        out[v] = v_out
-    if table:
-        df = pd.DataFrame(columns=['Feature', 'Bin', 'WOE'])
-        for v in out:
-            for b in out[v]:
-                df = df.append({'Feature':v, 'Bin':b, 'WOE':out[v][b]}, ignore_index=True)
-        out = df
-    return out
+from .bins import bin_tree, bin_scatter
 
 
 # raw value transfer to woe value according woe table
 def woe_replace(woes, data):
     '''Use woe value to replace original value.
-
+    Args:
+        -woe : woe value dictionary or pandas data frame.
+        -data : raw data used to replace with woe value.
     '''
     if isinstance(woes, dict):
         cols = woes.keys()
     elif isinstance(woes, pd.DataFrame):
-        cols = woes['Feature'].unique()
+        if sum(woes.columns.isin(['Variable', 'Bin', 'WOE'])) != 3:
+            raise ValueError('DataFrame should contains Variable, Bin and WOE')
+        tmp = {}
+        for i, row in woes.iteritems():
+            var = row['Variable']
+            border = row['Bin']
+            woe = row['WOE']
+            if var not in tmp:
+                tmp[var] = {}
+            tmp[var][border] = [woe]
+        woes = tmp
+    else:
+        raise TypeError("WOE should be dictionary or pandas DataFrame.")
 
     woe_data = data.loc[:, data.columns.isin(cols)].copy()
     for var in woes:
@@ -76,7 +43,7 @@ def woe_replace(woes, data):
         for border in var_woe:
             start, end = pd.to_numeric(border.split(':'))
             flags = ((woe_data[var]>= start) & (woe_data[var]<end))
-            woe_data.loc[flags, var] = var_woe[border]
+            woe_data.loc[flags, var] = var_woe[border][0]
     return woe_data
 
 
@@ -111,8 +78,82 @@ def iv(x, y, good_label=0, verbose=False):
                     border_woe, 
                     iv_i))
         iv += iv_i
-
     return iv
+
+
+# 计算 IV 值
+def woe_iv(data, y, vars=None, good_label=0, dt=None, min_samples_node=0.05, na_omit=True, 
+           verbose=False, **kwargs):
+    '''Calculate feature iv value.
+    Args:
+        data: pandas data frame.
+        y: target value column name. 
+        vars: variables, list like.
+        good_label: label for good case, default is 0.
+        dt: list like contains 0 and 1, the length is the same as the number of variables. 0 
+        indicates numberic data, 1 indicates category data.
+        na_omit: remove NAN value, default is True.
+        verbose: print verbose information.
+    Returns:
+        A pandas data frame is returned. Contains variable, bin, good/bad case number 
+        and corresponding woe and iv value.
+    '''
+    if len(set(data[y]))!= 2:
+        raise TypeError('Need binary value in label.')
+
+    bad_label = list(set(data[y]) - set([good_label]))[0]
+
+    if vars == None:
+        vars = data.columns.tolist()
+        vars.remove(y)
+
+    if dt == None or len(dt) != len(vars):
+        raise TypeError("dt argument is a list contains 0 and 1, \
+                    whose length equals variable's length.")
+
+    info_df = pd.DataFrame(columns=['Variables', 'Bin', 'Good', 'Bad', 'pnt_%s'% good_label, 
+                            'pnt_%s' % bad_label, 'woe', 'iv_i'])
+
+    for idx, var in enumerate(vars):
+        if verbose:
+            print("Processing on %s" % var)
+
+        if na_omit:
+            total_good = float(sum(data[y][~pd.isna(data[var])] == good_label))
+            total_bad = len(data[y][~pd.isna(data[var])]) - total_good
+        else:
+            total_good = float(sum(data[y]==good_label))
+            total_bad = data.shape[0] - total_good
+
+        if dt[idx] == 0:
+            bins_out = bin_tree(data[var], data[y], na_omit=na_omit, min_samples_node= min_samples_node, **kwargs)
+        elif dt[idx] == 1:
+            bins_out = bin_scatter(data[var])
+            
+        #print(bins_out.keys())
+        if verbose:
+            print("total_good: %s\ttotal_bad: %s\n" % (total_good, total_bad))
+            print("Variables\tBin\tGood(%)\tBad(%)\twoe_i\tiv_i") 
+        
+        for border in bins_out:
+            border_good = float(sum(data[y][bins_out[border]]==good_label)) 
+            border_bad = len(bins_out[border]) - border_good
+            border_woe = log((border_bad/total_bad)/(border_good/total_good))
+            iv_i = (border_bad/total_bad - border_good/total_good) * border_woe
+            row = [var,
+                    border,
+                    border_good,
+                    border_bad,
+                    border_good/total_good , 
+                    border_bad/total_bad,
+                    border_woe, 
+                    iv_i]
+            info_df.loc[info_df.shape[0], :] = row
+            if verbose:
+                print('\t'.join([str(_i) for _i in row]))
+    iv = info_df.groupby('Variables').iv_i.sum()
+    info_df['iv'] = info_df['Variables'].map(lambda x:iv[x])
+    return info_df
 
 
 def getConstant(theta, pdo, basescore, data, woe_table, verbose=False):
@@ -143,6 +184,8 @@ def getConstant(theta, pdo, basescore, data, woe_table, verbose=False):
     if verbose:
         print("Shift is %s, slope is %s" % (shift, slope))
     return (shift, slope)
+
+def getScoreCard(woe_table):
 
 
 def getScore(woe_table, xarray, missing=0):
